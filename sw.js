02c2a29f7fca427676f2b10c776f8b084efab950
Fadeflux <fadefluxagency@gmail.com>
2026-07-05 01:10:34 +0200

    Shinra Finance - frontend (GitHub Pages)

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE = 'shinra-v1';
+const ASSETS = ['/crm-agency-frontend/', '/crm-agency-frontend/index.html'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS).catch(() => {})));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  if (e.request.url.includes('/api/')) return;
+  e.respondWith(
+    fetch(e.request).then(res => {
+      const clone = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, clone));
+      return res;
+    }).catch(() => caches.match(e.request))
+  );
+});
